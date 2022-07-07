541a04dae80ec804b735e89530af2d6c040e2f59
BoiseITGuru <dev@boiseitguru.com>
2022-07-07 15:48:55 -0600

    Created iOS Project

---
+++ Boise's Finest DAO/Boise_s_Finest_DAOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boise_s_Finest_DAOApp.swift
+//  Boise's Finest DAO
+//
+//  Created by Brian Pistone on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Boise_s_Finest_DAOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boise's Finest DAO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Boise's Finest DAO
+//
+//  Created by Brian Pistone on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
